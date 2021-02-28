6b636b4ba976f2c8b2e74553c82e7c42dc9fd321
ritwikchakraborty123 <ritwikchakraborty.ritwik@gmail.com>
2021-02-28 13:24:47 +0530

    2 files modified

--- src/app/todo.componenet.ts
+++ src/app/todo.componenet.ts
@@ -9,6 +9,8 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 export class todoComponent {
   record = [];
   name;
+  open=[];
+  status=[];
   todoForm = new FormGroup({
     shortdescription: new FormControl(),
     title: new FormControl(),
@@ -18,13 +20,19 @@ export class todoComponent {
     this.name = "The Todo List";
   }
   onSubmit() {
+    this.open.push(false);
+    this.status.push(0);
     this.record.push(this.todoForm.value);
     console.log(this.todoForm.value);
   }
-  onClick($e){
-     var target = event.target || event.srcElement || event.currentTarget;
-    console.log($e);
-    console.log(target);
+  onClick(e,ind){
+    //  var target = event.target || event.srcElement || event.currentTarget;
+    this.open[ind]=this.open[ind]^1;
+    
     
   }
+  changestatus(e,ind){
+    this.status[ind]+=1;
+  }
+  
 }
